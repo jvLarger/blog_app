a8e3815b163e61cb6cdbb903c276e732cd36550b
João Victor <44883992+jvLarger@users.noreply.github.com>
2025-10-25 19:50:05 -0300

    Add current user session support to auth feature
    Introduces the ability to fetch the current logged-in user by adding a new use case, repository method, and data source method. Updates the AuthBloc to handle a new AuthIsUserLoggedIn event, triggering user session checks on app start. Also adds a NoParams class for use cases and updates dependency injection to register the new use case.

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,5 @@
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
@@ -10,14 +12,37 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
   final UserLogin _userLogin;
-  AuthBloc({
-    required UserSignUp userSignUp,
-    required UserLogin userLogin,
-  })  : _userSignUp = userSignUp,
+  final CurrentUser _currentUser;
+  AuthBloc(
+      {required UserSignUp userSignUp,
+      required UserLogin userLogin,
+      required CurrentUser currentUser})
+      : _userSignUp = userSignUp,
         _userLogin = userLogin,
+        _currentUser = currentUser,
         super(AuthInitial()) {
     on<AuthSignUp>(_onAuthSignUp);
     on<AuthLogin>(_onAuthLogin);
+    on<AuthIsUserLoggedIn>(_isUserLoggedIn);
+  }
+
+  void _isUserLoggedIn(
+    AuthIsUserLoggedIn event,
+    Emitter<AuthState> emit,
+  ) async {
+    final response = await _currentUser(NoParams());
+    response.fold(
+      (failure) => emit(
+        AuthFailure(
+          message: failure.message,
+        ),
+      ),
+      (user) => emit(
+        AuthSuccess(
+          user: user,
+        ),
+      ),
+    );
   }
 
   void _onAuthSignUp(AuthSignUp event, Emitter<AuthState> emit) async {
