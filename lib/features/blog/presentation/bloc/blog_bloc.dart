75be6966f16a42e059bfdb5b932afb074526ebb9
João Victor <44883992+jvLarger@users.noreply.github.com>
2025-11-01 18:27:00 -0300

    Add blog upload feature with Bloc and Supabase
    Introduces the ability to upload blogs, including image upload, using a new BlogBloc, repository, use case, and data source integrated with Supabase. Adds form validation, Bloc state management, and dependency injection setup. Updates UI to support blog creation and navigation, and adds the uuid package for unique blog IDs.

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState> {
+  final UploadBlog uploadBlog;
+
+  BlogBloc(this.uploadBlog) : super(BlogInitial()) {
+    on<BlogEvent>((event, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+  }
+
+  void _onBlogUpload(BlogUpload event, Emitter<BlogState> emit) async {
+    final response = await uploadBlog(
+      UploadBlogParams(
+        posterId: event.posterId,
+        title: event.title,
+        content: event.content,
+        image: event.image,
+        topics: event.topics,
+      ),
+    );
+
+    response.fold(
+      (failure) => emit(
+        BlogFailure(
+          failure.message,
+        ),
+      ),
+      (blog) => emit(
+        BlogSuccess(),
+      ),
+    );
+  }
+}
